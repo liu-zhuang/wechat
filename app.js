099231adb56ff3655e16d02c9d640f8f53a2d1a0
nickylau82 <70458055@qq.com>
2017-09-13 23:32:00 +0800

    modify app.js

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-var Koa = require('koa');
-var app = new Koa();
-var router = require('./controller.js')();
+// var Koa = require('koa');
+// var app = new Koa();
+// var router = require('./controller.js')();
 
-var sha1 = require('sha1');
-var bodyParser = require('koa-bodyparser');
+// var sha1 = require('sha1');
+// var bodyParser = require('koa-bodyparser');
 
-app.use(async (ctx,next) => {
-	console.log(new Date());
-	await next ();
+// app.use(async (ctx,next) => {
+// 	console.log(new Date());
+// 	await next ();
+// });
+
+
+// app.use(bodyParser());
+// app.use(router);
+
+
+
+// app.listen(3000);
+// console.log('listening port 3000');
+// 
+let http = require('http');
+
+let server = http.createServer((req, res) => {
+	res.writeHead(200, {
+		'Content-type': "text/plain"
+	})
+	res.write('hello pm2');
+	res.end();
 });
-
-
-app.use(bodyParser());
-app.use(router);
-
-
-
-app.listen(3000);
-console.log('listening port 3000');
+server.listen(3000);
