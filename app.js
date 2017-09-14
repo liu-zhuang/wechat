c2f1f9e6dd13b1bf2e170ebc1f23b03bd11fbdbb
nickylau82 <70458055@qq.com>
2017-09-14 19:51:59 +0800

    rollback

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var Koa = require('koa');
 var app = new Koa();
-// var router = require('./controller.js')();
+var router = require('./controller.js')();
 
 var sha1 = require('sha1');
 var bodyParser = require('koa-bodyparser');
 
-// app.use(async (ctx,next) => {
-// 	console.log(new Date());
-// 	await next ();
-// });
+app.use(async (ctx,next) => {
+	console.log(new Date());
+	await next ();
+});
 
 
 app.use(bodyParser());
