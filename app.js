e6e4b2fb46bc475a7317411e53f0830fcfb9852c
nickylau82 <70458055@qq.com>
2017-09-14 19:11:35 +0800

    use koa2

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-// var Koa = require('koa');
-// var app = new Koa();
-// var router = require('./controller.js')();
+var Koa = require('koa');
+var app = new Koa();
+var router = require('./controller.js')();
 
-// var sha1 = require('sha1');
-// var bodyParser = require('koa-bodyparser');
+var sha1 = require('sha1');
+var bodyParser = require('koa-bodyparser');
 
-// app.use(async (ctx,next) => {
-// 	console.log(new Date());
-// 	await next ();
-// });
-
-
-// app.use(bodyParser());
-// app.use(router);
-
-
-
-// app.listen(3000);
-// console.log('listening port 3000');
-// 
-let http = require('http');
-
-let server = http.createServer((req, res) => {
-	res.writeHead(200, {
-		'Content-type': "text/plain"
-	})
-	res.write('hello pm2  this is new');
-	res.end();
+app.use(async (ctx,next) => {
+	console.log(new Date());
+	await next ();
 });
-server.listen(3000);
+
+
+app.use(bodyParser());
+app.use(router);
+
+
+
+app.listen(3000);
 console.log('listening port 3000');
+
+// let http = require('http');
+
+// let server = http.createServer((req, res) => {
+// 	res.writeHead(200, {
+// 		'Content-type': "text/plain"
+// 	})
+// 	res.write('hello pm2  this ');
+// 	res.end();
+// });
+// server.listen(3000);
+// console.log('listening port 3000');
