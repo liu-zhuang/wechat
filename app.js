b3349fe677a5f051ed444c287f7c0dfe3f323440
liuzhuang <70458055@qq.com>
2017-09-20 11:23:25 +0800

    modify app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const Sha = require('sha1');
 const route = require('koa-route');
+const wechat = require('./middleware/wechat');
 
 const app = new Koa();
 
@@ -9,33 +10,16 @@ const main = ctx => {
 	ctx.response.body = '<h1>wechat</h1>'
 }
 const config = {
-	token: 'yebaomemeda'
-};
-
-const check = ctx => {
-	//console.log('query:'+ctx.query);
-	// console.log('nonce:'+ctx.query.nonce);
-	console.log(ctx.query.signature);
-	let signature = ctx.query.signature;
-	let timestamp = ctx.query.timestamp;
-	let nonce = ctx.query.nonce;
-	let echostr = ctx.query.echostr;
-	let sort = [config.token, timestamp, nonce].sort().join('');
-	let sha1 = Sha(sort);
-	console.log(sort);
-	console.log(sha1);
-	if (sha1 === signature) {
-		console.log('success');
-		ctx.response.body = echostr;
-	} else {
-		console.log('fail');
-		ctx.response.body = 'wrong';
+	wechat: {
+		token: 'yebaomemeda'	
 	}
-}
+	
+};
 
 
 app.use(route.get('/', main));
-app.use(route.get('/check', check));
+app.use(route.get('/check', wechat.check(config.wechat)));
+app.use(route.get('/getAccessToken', wechat.getAccessToken);
 
 app.listen(3000);
 
